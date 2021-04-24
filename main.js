d89dd8720fdc527923f097bb60fe0fa17e814992
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-04-24 10:22:52 +0530

    Updated with corrected code

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function getParagraph1()
 {
     var inputs = [];
     for(var i=1; i <=6 ; i++){
-        inputs.push(document.getElementById("para1_input_box_" + 1).value);
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
     }
-    inputs.join(".");
-    document.getElementById("showParagraph1").innerHTML = inputs.join(".");
+    inputs.join(". ");
+    document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
 }
